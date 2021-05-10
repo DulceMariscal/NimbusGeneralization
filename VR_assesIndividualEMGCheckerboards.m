42fd3f801c8af84ae24051c470c0cdfc1de67ce0
dum5 <37150306+dum5@users.noreply.github.com>
2021-05-10 15:01:42 -0400

    Update VR_assesIndividualEMGCheckerboards.m

--- VR_assesIndividualEMGCheckerboards.m
+++ VR_assesIndividualEMGCheckerboards.m
@@ -1,6 +1,6 @@
 % clear; close all;
 
-sub={'VROG_03params'};
+sub={'CVROG_01params'};
 
 normalizedTMFullAbrupt=adaptationData.createGroupAdaptData(sub);
 
@@ -165,81 +165,81 @@ set(gcf,'color','w');
 % Adapt - baseline
 % OGpost - ADapt SS
 
-sub={'VROG_02params'};
+% sub={'VROG_02params'};
 
-normalizedTMFullAbrupt=adaptationData.createGroupAdaptData(sub);
-
-ss =normalizedTMFullAbrupt.adaptData{1}.data.getLabelsThatMatch('^Norm');
-
-% ss =TMFullAbrupt.adaptData{1}.data.getLabelsThatMatch('^Norm'); %Dulce 
-s2 = regexprep(ss,'^Norm','dsjrs');
-normalizedTMFullAbrupt=normalizedTMFullAbrupt.renameParams(ss,s2);
-% normalizedTMFullAbrupt=studyData.TMFullAbrupt.renameParams(ss,s2);
-
-% muscleOrder={'TA','MG','SEMT','VL','RF'};
-muscleOrder={'TA', 'PER', 'SOL', 'LG', 'MG', 'BF', 'SEMB', 'SEMT', 'VM', 'VL', 'RF', 'TFL', 'GLU', 'HIP'};
-% muscleOrder={'TA', 'SOL', 'LG', 'MG', 'BF', 'SEMB', 'SEMT', 'VM', 'VL', 'RF', 'TFL', 'GLU', 'HIP'};
-
-n_muscles = length(muscleOrder);
-useLateAdaptAsBaseline=false;
-
-n_subjects = 1;
-extremaMatrixYoung = NaN(n_subjects,n_muscles * 2,2);
-
-ep=defineEpocVR_OG_Regressor('nanmean');
-% refEp = defineReferenceEpoch('OGNimbus',ep);
-refEpAdaptLate = defineReferenceEpoch('Adaptation',ep);
-refEpOGBase=defineReferenceEpoch('SplitPos',ep);
-% refEpShortNeg=defineReferenceEpoch('SplitNeg',ep);
-refEpTM = defineReferenceEpoch('TMbase',ep);
-% refEp = defineEpochYoungLongAdaptation('Fast',ep);
-
-newLabelPrefix = defineMuscleList(muscleOrder);
-
-% normalizedTMFullAbrupt = normalizedTMFullAbrupt.normalizeToBaselineEpoch(newLabelPrefix,ep(1,:));
-normalizedTMFullAbrupt = normalizedTMFullAbrupt.normalizeToBaselineEpoch(newLabelPrefix,refEp);
-
-
-ll=normalizedTMFullAbrupt.adaptData{1}.data.getLabelsThatMatch('^Norm');
-%ll = normalizedTMFullAbrupt.adaptData{1}.data.getLabelsThatMatch('^(s|f)[A-Z]+_s');
-
-l2=regexprep(regexprep(ll,'^Norm',''),'_s','s');
-normalizedTMFullAbrupt=normalizedTMFullAbrupt.renameParams(ll,l2);
-
-newLabelPrefix = regexprep(newLabelPrefix,'_s','s');
-
-
-for i = 1:n_subjects
-    
-
-    adaptDataSubject = normalizedTMFullAbrupt.adaptData{1, i}; 
-
-    fh=figure('Units','Normalized','OuterPosition',[0 0 1 1]);
-    ph=tight_subplot(1,4,[.03 .005],.04,.04);
-    flip=true;
-    
-     
-    adaptDataSubject.plotCheckerboards(newLabelPrefix,ep(2,:),fh,ph(1,2),refEpOGBase,flip); % baseline TM - EMG_split(+) 
-    adaptDataSubject.plotCheckerboards(newLabelPrefix,ep(4,:),fh,ph(1,1),refEpTM,flip); %  EMG_split(-) - baseline TM
-    adaptDataSubject.plotCheckerboards(newLabelPrefix,ep(6,:),fh,ph(1,3),refEpAdaptLate,flip); %OG base - Adapt SS
-    adaptDataSubject.plotCheckerboards(newLabelPrefix,ep(8,:),fh,ph(1,4),refEpAdaptLate,flip); %OGafter - Adaptation_{SS} 
-%     [~,~,labels,dataE{1},dataRef{1}]=adaptDataSubject.plotCheckerboards(newLabelPrefix,ep,fh,ph(1,2:end),refEp,flip);%Second, the rest:
+% normalizedTMFullAbrupt=adaptationData.createGroupAdaptData(sub);
+% 
+% ss =normalizedTMFullAbrupt.adaptData{1}.data.getLabelsThatMatch('^Norm');
+% 
+% % ss =TMFullAbrupt.adaptData{1}.data.getLabelsThatMatch('^Norm'); %Dulce 
+% s2 = regexprep(ss,'^Norm','dsjrs');
+% normalizedTMFullAbrupt=normalizedTMFullAbrupt.renameParams(ss,s2);
+% % normalizedTMFullAbrupt=studyData.TMFullAbrupt.renameParams(ss,s2);
+% 
+% % muscleOrder={'TA','MG','SEMT','VL','RF'};
+% muscleOrder={'TA', 'PER', 'SOL', 'LG', 'MG', 'BF', 'SEMB', 'SEMT', 'VM', 'VL', 'RF', 'TFL', 'GLU', 'HIP'};
+% % muscleOrder={'TA', 'SOL', 'LG', 'MG', 'BF', 'SEMB', 'SEMT', 'VM', 'VL', 'RF', 'TFL', 'GLU', 'HIP'};
+% 
+% n_muscles = length(muscleOrder);
+% useLateAdaptAsBaseline=false;
+% 
+% n_subjects = 1;
+% extremaMatrixYoung = NaN(n_subjects,n_muscles * 2,2);
+% 
+% ep=defineEpocVR_OG_Regressor('nanmean');
+% % refEp = defineReferenceEpoch('OGNimbus',ep);
+% refEpAdaptLate = defineReferenceEpoch('Adaptation',ep);
+% refEpOGBase=defineReferenceEpoch('SplitPos',ep);
+% % refEpShortNeg=defineReferenceEpoch('SplitNeg',ep);
+% refEpTM = defineReferenceEpoch('TMbase',ep);
+% % refEp = defineEpochYoungLongAdaptation('Fast',ep);
+% 
+% newLabelPrefix = defineMuscleList(muscleOrder);
+% 
+% % normalizedTMFullAbrupt = normalizedTMFullAbrupt.normalizeToBaselineEpoch(newLabelPrefix,ep(1,:));
+% normalizedTMFullAbrupt = normalizedTMFullAbrupt.normalizeToBaselineEpoch(newLabelPrefix,refEp);
+% 
+% 
+% ll=normalizedTMFullAbrupt.adaptData{1}.data.getLabelsThatMatch('^Norm');
+% %ll = normalizedTMFullAbrupt.adaptData{1}.data.getLabelsThatMatch('^(s|f)[A-Z]+_s');
+% 
+% l2=regexprep(regexprep(ll,'^Norm',''),'_s','s');
+% normalizedTMFullAbrupt=normalizedTMFullAbrupt.renameParams(ll,l2);
+% 
+% newLabelPrefix = regexprep(newLabelPrefix,'_s','s');
+% 
+% 
+% for i = 1:n_subjects
+%     
+% 
+%     adaptDataSubject = normalizedTMFullAbrupt.adaptData{1, i}; 
+% 
+%     fh=figure('Units','Normalized','OuterPosition',[0 0 1 1]);
+%     ph=tight_subplot(1,4,[.03 .005],.04,.04);
+%     flip=true;
+%     
+%      
+%     adaptDataSubject.plotCheckerboards(newLabelPrefix,ep(2,:),fh,ph(1,2),refEpOGBase,flip); % baseline TM - EMG_split(+) 
+%     adaptDataSubject.plotCheckerboards(newLabelPrefix,ep(4,:),fh,ph(1,1),refEpTM,flip); %  EMG_split(-) - baseline TM
+%     adaptDataSubject.plotCheckerboards(newLabelPrefix,ep(6,:),fh,ph(1,3),refEpAdaptLate,flip); %OG base - Adapt SS
+%     adaptDataSubject.plotCheckerboards(newLabelPrefix,ep(8,:),fh,ph(1,4),refEpAdaptLate,flip); %OGafter - Adaptation_{SS} 
+% %     [~,~,labels,dataE{1},dataRef{1}]=adaptDataSubject.plotCheckerboards(newLabelPrefix,ep,fh,ph(1,2:end),refEp,flip);%Second, the rest:
+% %    
+%     
+%     
+%     set(ph(:,1),'CLim',[-1 1]);
+% %     set(ph(:,2:end),'YTickLabels',{},'CLim',[-1 1]*2);
+%     set(ph(:,2:end),'YTickLabels',{},'CLim',[-1 1]*1.5);
+%     set(ph,'FontSize',8)
+%     pos=get(ph(1,end),'Position');
+%     axes(ph(1,end))
+%     colorbar
+%     set(ph(1,end),'Position',pos);
+%     
 %    
-    
-    
-    set(ph(:,1),'CLim',[-1 1]);
-%     set(ph(:,2:end),'YTickLabels',{},'CLim',[-1 1]*2);
-    set(ph(:,2:end),'YTickLabels',{},'CLim',[-1 1]*1.5);
-    set(ph,'FontSize',8)
-    pos=get(ph(1,end),'Position');
-    axes(ph(1,end))
-    colorbar
-    set(ph(1,end),'Position',pos);
-    
-   
-    
-    extremaMatrixYoung(i,:,1) =  min(dataRef{1});
-    extremaMatrixYoung(i,:,2) =  max(dataRef{1});
-    
-end
-set(gcf,'color','w');
+%     
+%     extremaMatrixYoung(i,:,1) =  min(dataRef{1});
+%     extremaMatrixYoung(i,:,2) =  max(dataRef{1});
+%     
+% end
+% set(gcf,'color','w');
