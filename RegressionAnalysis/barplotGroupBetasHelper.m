80481bfa59083eb2f64da88b6abaff9f49288596
DanielleLiu <DanielleLiu@users.noreply.github.com>
2021-06-08 15:15:23 -0400

    Update code to compute legend index, xlim, and xtick locations
    - Support computing legend index, xlim and xtick locations automatically for various coefficient numbers

--- RegressionAnalysis/barplotGroupBetasHelper.m
+++ RegressionAnalysis/barplotGroupBetasHelper.m
@@ -23,16 +23,18 @@ function hfig = barplotGroupBetasHelper(coeff, groupIDs, transitionNum, normaliz
     hold on;
     colors = {'#0072BD','#D95319','#EDB120','#7E2F8E','#77AC30','#4DBEEE','#A2142F'};
     num_coeff = length(coeff{1}.Estimate);
-
+    
+    xtickloc = nan(1,length(groupIDs));
     for i = 1:length(groupIDs)
         for j = 1 :num_coeff
             bar(i*(num_coeff+1) + j , coeff{i}.Estimate(j),'FaceColor',colors{j},'EdgeColor',colors{j});%,'.','Color',colors{i},'MarkerSize',40)
             %x, bary, errorlow, errorhigh, the error is absolute value from the bar height   
-            er = errorbar(i*(num_coeff+1) + j,coeff{i}.Estimate(j),coeff{i}.SE(j),coeff{i}.SE(j), 'Color','k','LineWidth',3); 
+            errorbar(i*(num_coeff+1) + j,coeff{i}.Estimate(j),coeff{i}.SE(j),coeff{i}.SE(j), 'Color','k','LineWidth',3); 
         end
+        xtickloc(i) = i*(num_coeff+1) + j/2 + 0.5;
     end
 
-    set(gca,'XLim',[1*(num_coeff+1) (length(groupIDs)+1)*(num_coeff+1)],'XTick',[6,10,15,18,22],'XTickLabel',groupIDs);
+    set(gca,'XLim',[1*(num_coeff+1) (length(groupIDs)+1)*(num_coeff+1)],'XTick',xtickloc,'XTickLabel',groupIDs);
     set(gca,'FontSize',18);
     ylabel(['\beta at Transition' transitionNum]);
     title(['Regression Coefficients at Transition ' num2str(transitionNum)]);
@@ -40,7 +42,12 @@ function hfig = barplotGroupBetasHelper(coeff, groupIDs, transitionNum, normaliz
     f= get(gca,'Children');
     legendLabels = coeff{1}.Row;
     legendLabels{end+1} = 'SE (Group Regression)';
-    legend(f([end,end-length(groupIDs)*2,end-length(groupIDs)*4, end-1]),legendLabels); %item plotted 1st, 10th and 20th
+    legendidx = nan(1, num_coeff);
+    for i = 1:num_coeff
+        legendidx(i) = length(f)-2*(i-1);
+    end
+    legendidx(num_coeff+1) = length(f) - 1; %SE legend
+    legend(f(legendidx),legendLabels); %item plotted 1st, 10th and 20th
     
     if nargin == 5 %no result directory provided, avoid saving
         if not(isfolder(resultDir))
