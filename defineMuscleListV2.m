1600e636985c99ab143de40694c70b0eaac2c99d
DulceMariscal <mariscaldulce@gmail.com>
2024-04-04 15:40:57 -0400

    defining muscle list
    - I think this function is not longer in use

---
+++ defineMuscleListV2.m
@@ -0,0 +1,7 @@
+function labelPrefix = defineMuscleListV2(mOrder)
+
+    labelPrefix=([strcat('f',mOrder) strcat('s',mOrder)]); %To display fast and slow
+%     labelPrefix = strcat(labelPrefix);
+    
+end
+
